Fixed issue with encoders and normalization

cheatsheet_ai_proj/cheatsheet_ai/compile_data.py
@@ -1,3 +1,6 @@
+import copy
+import itertools
+import statistics
 from models import Graph
 
 import json
@@ -199,11 +202,8 @@ def extract_edge_params(edge_elements: List[ET.Element])->Tuple[List[str], List[
 def generate_adjacency_matrix(ids: List[str], source_ids: List[str], target_ids: List[str])->List[Tuple[int, int]]:
     # Returns adjacency matrix in COO form [Coordinate Form]
     id2index: Dict[str, int] = {ids[i]: i for i in range(len(ids))}
+    
     # Ignore edges with source or target ids that is not found in list
-    print(ids)
-    print(source_ids)
-    print(target_ids)
-
     filtered = tuple(zip(*filter(lambda ids: ids[0] in id2index and ids[1] in id2index, zip(source_ids, target_ids))))
     source_ids, target_ids = filtered
     #print(len(t))
@@ -218,17 +218,46 @@ def drawio2graph(drawio_file_path: str)->Graph:
     xml: ET.ElementTree = parse_drawio_as_xml(drawio_file_path)
     shape_elements, edge_elements = extract_shape_and_edge_elements(xml)
 
-    shape_ids, shape_titles, xs, ys, widths, heights = extract_shape_params(shape_elements)
+    shape_ids, shape_titles, xs, ys, widths, heights = extract_shape_params(shape_elements)    
     edge_styles, source_ids, target_ids, edge_points = extract_edge_params(edge_elements)
+    
+    old_shape_ids = copy.deepcopy(shape_ids)
+    # Remove shapes without any titles
+    shape_ids, shape_titles, xs, ys, widths, heights = zip(*filter(lambda d: d[1]!='',  
+                                                                   zip(*[shape_ids, shape_titles, xs, ys, widths, heights])))
+               
+    print(len(old_shape_ids)-len(shape_ids))
     coo_adjacency_matrix:np.array = generate_adjacency_matrix(shape_ids, source_ids, target_ids)
-    return Graph(shape_ids, shape_titles, xs, ys, widths, heights, coo_adjacency_matrix)
+    
+    from train import compute_edge_vectors
+    import torch
+    from torch_geometric.utils import to_torch_sparse_tensor
+    # import ipdb; ipdb.set_trace()
+    # coo_adjacency_matrix = torch.LongTensor(coo_adjacency_matrix).T
+    sparse_adjacency_matrix: torch.Tensor = to_torch_sparse_tensor(torch.LongTensor(coo_adjacency_matrix).T, size=len(xs))
+    xs = torch.FloatTensor(xs)
+    ys = torch.FloatTensor(ys)
+    # print(shape_titles[((xs[coo_adjacency_matrix[0]]==xs[coo_adjacency_matrix[1]]).to(torch.long) + (ys[coo_adjacency_matrix[0]]==ys[coo_adjacency_matrix[1]].to(torch.long))).to(torch.bool).tolist().index(True)])
+    edge_vectors = compute_edge_vectors(xs, ys, sparse_adjacency_matrix)
+    # print(edge_vectors.detach()[sparse_adjacency_matrix.to(torch.bool).to_dense()].to('cpu').flatten().tolist().count(0)/ \
+    #     len(edge_vectors[sparse_adjacency_matrix.to(torch.bool).to_dense()].flatten()) )
+    # if edge_vectors.detach()[sparse_adjacency_matrix.to(torch.bool).to_dense()].to('cpu').flatten().tolist().count(0)/ \
+    #     len(edge_vectors[sparse_adjacency_matrix.to(torch.bool).to_dense()].flatten()) > 0.7:
+    #     print(drawio_file_path)
+        
+    return Graph(shape_ids, shape_titles, xs.tolist(), ys.tolist(), widths, heights, coo_adjacency_matrix)
 
 def make_dataset():
-    dataset_dir: str = 'drawio_dataset'
+    # dataset_dir: str = 'cheatsheet_ai_proj/overfit'
+    dataset_dir: str = 'cheatsheet_ai_proj/drawio_dataset'
     drawio_file_names: List[str] = os.listdir(dataset_dir)
     graphs: List[Graph] = [drawio2graph(f'{dataset_dir}/{drawio_file_names[i]}') for i in range(len(drawio_file_names))]
-    with open('graph_dataset.json', 'w') as handle:
+    with open('cheatsheet_ai_proj/graph_dataset.json', 'w') as handle:
         json.dump(graphs, handle)
+    
+    print('max width: ', max(itertools.chain(*[graph.widths for graph in graphs])))
+    print('max height: ', max(itertools.chain(*[graph.heights for graph in graphs])))
+
  
 if __name__ == '__main__':  
     make_dataset()

cheatsheet_ai_proj/cheatsheet_ai/eval.py
@@ -6,7 +6,7 @@ from train import GCN
 import xml.etree.ElementTree as ET
 
 import torch
-from torch import FloatTensor, LongTensor
+from torch import FloatTensor, LongTensor, norm
 from torch_geometric.data import Data
 from transformers import BertTokenizer
 
@@ -34,26 +34,37 @@ def update_drawio(ids, xs, ys, widths, heights, drawio_file_path: str, save_path
         if shape_element is None:
             raise ValueError('Could not find the shape element of the id')
 
-        # shape_element.attrib['x'] = str(xs[i])
-        # shape_element.attrib['y'] = str(ys[i])
+        shape_element.attrib['x'] = str(xs[i])
+        shape_element.attrib['y'] = str(ys[i])
         shape_element.attrib['width'] = str(widths[i])
         shape_element.attrib['height'] = str(heights[i])
 
     tree.write(save_path)
 
 if __name__ == '__main__':
-    source_drawio_file = 'test.drawio'
+    source_drawio_file = 'cheatsheet_ai_proj/cheatsheet_ai/test.drawio'
     graph: Graph = drawio2graph(source_drawio_file)
     tokenizer: BertTokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     data: Data = graph2data(graph.titles, graph.xs, graph.ys, graph.widths, graph.heights, 
                             graph.coo_adjacency_matrix, tokenizer)
+    normalize_width = 140
+    normalize_height = 80
     model = GCN()
     with torch.no_grad():
         model.eval()
         model.load_state_dict(torch.load('model.torch'))
         out: FloatTensor = model(data)
-        out = out.T    
+        out = out.T
+        
+    print('x')
+    print(out[0].tolist())
+    print('y')
+    print(out[1].tolist())
+    print('width')  
     print(out[2].tolist())
+    print('height')
     print(out[3].tolist())
+    # update_drawio(graph.ids, out[0].tolist(), out[1].tolist(), out[2].tolist(), out[3].tolist(), 
+    #               source_drawio_file, 'cheatsheet_ai_proj/cheatsheet_ai/test2.drawio')
     update_drawio(graph.ids, out[0].tolist(), out[1].tolist(), out[2].tolist(), out[3].tolist(), 
-                  source_drawio_file, 'test2.drawio')
+                  source_drawio_file, 'cheatsheet_ai_proj/cheatsheet_ai/test2.drawio')

cheatsheet_ai_proj/cheatsheet_ai/generate_dataset.py
@@ -1,9 +1,9 @@
+import itertools
 import json
-from typing import Dict, List, Tuple
+from typing import Callable, Dict, List, Tuple
+from random import choice
 
-from responses import target
 from models import Graph
-import math
 
 import numpy as np
 import torch
@@ -15,20 +15,41 @@ from transformers import BertModel, BertTokenizer
 from torch.nn import Dropout, ReLU
 from torch import nn
     
-def add_noise(data: np.array, sigma: List[int])->np.array:
+def bayesian_noise(data: np.array, sigma: float)->np.array:
     noise = np.random.normal(np.zeros(len(data)), sigma)
     scale_noise = np.random.uniform(0, 1)
     noise = scale_noise*noise
     data += noise
     return data
 
-def noise_node_params(xs: List[float], ys: List[float], 
-                            widths: List[float], heights: List[float])-> Tuple[List[float], List[float], List[float], List[float]]:
+def uniform_noise(data: List[float], spread: float)->np.array:
+    noise = -spread/2 - np.random.random(size=len(data))*(spread/2)
+    data += noise
+    return data
+
+def constant(data: List[float], spread: float)->np.array:
+    noise = -spread/2 - np.random.random()*(spread/2)
+    data = np.array([noise for i in range(len(data))])
+    return data
+
+def unconditionally_random(data: List[float], spread: float)->np.array:
+    noise = -spread/2 - np.random.random(size=len(data))*(spread/2)
+    return noise
+
+def all_functions_mixed(data: List[float], spread: float)->np.array:
+    noise_function = choice([bayesian_noise, uniform_noise, constant, unconditionally_random])
+    return noise_function(data, spread)
+
+def noise_node_params(  xs: List[float], ys: List[float], 
+                        widths: List[float], heights: List[float])-> Tuple[List[float], List[float], List[float], List[float]]:
     '''Add noise to the positions and the dimensions of the nodes.'''
-    xs = add_noise(np.array(xs), 1000)
-    ys = add_noise(np.array(ys), 1000)
-    widths = add_noise(np.array(widths), 200)
-    heights = add_noise(np.array(heights), 200)
+    noise_functions: List[Callable[[List[float], float], np.array]] = [bayesian_noise, uniform_noise, constant, 
+                                                                       unconditionally_random, all_functions_mixed]
+    noise_func: Callable[[List[float], float], np.array] = choice(noise_functions)
+    xs = noise_func(np.array(xs), 1000)
+    ys = noise_func(np.array(ys), 1000)
+    widths = noise_func(np.array(widths), 200)
+    heights = noise_func(np.array(heights), 200)
     return xs.tolist(), ys.tolist(), widths.tolist(), heights.tolist()
 
 def compute_token_char_length(tokens: List[int], tokenizer: BertTokenizer) -> List[int]:
@@ -55,13 +76,16 @@ def node_params2tensors(titles: List[str], xs: List[float], ys: List[float],
      
     return torch.hstack([tokens['input_ids'], tokens['attention_mask'], xs, ys, widths, heights, token_char_lengths])
 
-def graph2data(titles: List[str], xs:FloatTensor, ys:FloatTensor, widths:FloatTensor, 
-               heights:FloatTensor, coo_adjacency_matrix: LongTensor, tokenizer)->Data:
+# NOTE: MOVE THIS CODE SOMEWHERE ELSE. IT IS NOT USED HERE MAYBE IT's USED IN EVAL
+def graph2data(titles: List[str], xs: np.array, ys: np.array, widths: np.array, 
+               heights: np.array, coo_adjacency_matrix: LongTensor, tokenizer)->Data:
     input_tensor = node_params2tensors(titles, xs, ys, widths, heights, tokenizer)
-    targets = torch.Tensor([xs, ys, widths, heights]).T
-    print(targets.shape)
+    targets = torch.stack([LongTensor(xs), LongTensor(ys), LongTensor(widths), LongTensor(heights)]).T
+    # print('Heights', heights)
+    # print('LongTensor', LongTensor(heights))
+    # print(targets.shape)
     # What does .t().contiguous() do?
-    data: Data = Data(x=input_tensor, y=targets, edge_index=LongTensor(coo_adjacency_matrix).t().contiguous())
+    data: Data = Data(x=input_tensor, edge_index=LongTensor(coo_adjacency_matrix).t().contiguous())
     return data
     
     
@@ -69,22 +93,28 @@ def graph2train_data(graph: Graph)->Data:
     # Embed each nodes into vectors
     noised_xs, noised_ys, noised_widths, noised_heights = noise_node_params(graph.xs, graph.ys, graph.widths, graph.heights)
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    return graph2data(graph.titles, noised_xs, noised_ys, noised_widths, noised_heights, graph.coo_adjacency_matrix, tokenizer)
+    input_tensor = node_params2tensors(graph.titles, noised_xs, noised_ys, noised_widths, noised_heights, tokenizer)
+    targets = torch.stack([FloatTensor(graph.xs), FloatTensor(graph.ys), FloatTensor(graph.widths), FloatTensor(graph.heights)]).T
+    return Data(x=input_tensor, y=targets ,edge_index=LongTensor(graph.coo_adjacency_matrix).t().contiguous())
     
 def prepare_data()->List[Data]:
     ''' Convert the data in the graph_dataset.json to a Data instance of the torchG
     
     '''
     # Open Data File
-    with open('graph_dataset.json', 'r') as handle:
+    with open('cheatsheet_ai_proj/graph_dataset.json', 'r') as handle:
         json_data = json.load(handle)
     # Instantiate List of Tuple as a Graph instance
-    graphs: List[Graph] = [Graph(*d) for d in json_data]
+    copies_per_data = 3
+    # copies_per_data = 30
+    graphs: List[Graph] = itertools.chain(*[[Graph(*d) for d in json_data] for _ in range(copies_per_data)])
     # Convert Graph into Data Format
     data_list: List[Data] = []
     
     for graph in graphs:
+        # print('heights', graph.heights)
         train_data: Data = graph2train_data(graph)
+        # print('train_height', train_data.y[:, 3])
         data_list.append(train_data)
     return data_list
 
@@ -116,5 +146,5 @@ class MyOwnDataset(InMemoryDataset):
         
 
 if __name__ == '__main__':       
-    d = MyOwnDataset('/home/sp/Downloads/mindmap_ai/')
+    d = MyOwnDataset('cheatsheet_ai_proj/')
     print(d)

cheatsheet_ai_proj/cheatsheet_ai/main.py
@@ -23,7 +23,7 @@ def find_new_file_path(source_file_path)->str:
     
     
 if __name__ == '__main__':
-    drawio_file_path: str = input('Please enter drawio file path: ')
+    drawio_file_path: str = input('Please enter drawio file path: ').strip()
     if drawio_file_path[0] == "'" and drawio_file_path[-1] == "'":
         drawio_file_path = drawio_file_path[1:-1]
     graph: Graph = drawio2graph(drawio_file_path)
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     model = GCN()
     with torch.no_grad():
         model.eval()
-        model.load_state_dict(torch.load('model.torch'))
+        model.load_state_dict(torch.load('cheatsheet_ai_proj/cheatsheet_ai/model.torch'))
         out: FloatTensor = model(data)
         out = out.T
     new_file_path = find_new_file_path(drawio_file_path)